1ff87660c2a351cb87cd432b7c8a92d9a6bfa194
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-05-14 20:40:46 -0700

    MAY 14

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,40 @@
 /*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:03 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/13 21:56:06 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/14 19:24:19 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int		get_next_line(const int fd, char **line)
+{
+	int i;
+
+	i = 0;
+	*line = (char*)malloc(BUFF_SIZE);
+	while ((read(fd, &(*line)[i], 1) > 0) && i < BUFF_SIZE - 1)
+	{
+		if ((*line)[i] == '\n')
+			break;
+		i++;
+	}
+	(*line)[i - 1] = '\0';
+	return (1);
+}
+
+int main()
+{
+	int i;
+	int n;
+	char *p;
+	n = 0;
+	i = open("text.txt", O_RDONLY);
+	get_next_line(i, &p);
+	while (n++ < 5)
+	{
+		get_next_line(i, &p);
+		printf("%s\n", p);
+	}
+}
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,18 @@
 /*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:13 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/13 21:56:16 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/14 15:12:48 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1024
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int		get_next_line(const int fd, char **line);
+
+#endif
