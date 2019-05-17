8c53d480fffac5789ac081daf158ba06b9d3eb4a
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-05-16 23:11:48 -0700

    MAY 16

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:03 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/14 19:24:19 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/16 23:01:27 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,44 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	int i;
-
-	i = 0;
-	*line = (char*)malloc(BUFF_SIZE);
-	while ((read(fd, &(*line)[i], 1) > 0) && i < BUFF_SIZE - 1)
-	{
-		if ((*line)[i] == '\n')
+	int len;
+	char *p_n;
+	char *tmp;
+	char *str;
+	char buf[BUFF_SIZE + 1];
+	int ret;
+	
+	ret = 0;
+	len = 0;
+	str = ft_strnew(1);
+	while (read(fd, buf, BUFF_SIZE) > 0)
+	{	
+		buf[BUFF_SIZE] = '\0';
+		
+		tmp = str;
+		str = ft_strnew(len + BUFF_SIZE + 1);
+		len = ft_strlen(str);
+		ft_strcpy(str, tmp);
+		ft_strdel(&tmp);
+		ft_strcat(str, buf);	
+		if ((p_n = ft_strchr(buf, '\n')) != 0)
 			break;
-		i++;
+		ft_strclr(buf);
 	}
-	(*line)[i - 1] = '\0';
-	return (1);
+	*line = str;
+	return (ret);
 }
 
 int main()
 {
-	int i;
-	int n;
+	int fd;
 	char *p;
-	n = 0;
-	i = open("text.txt", O_RDONLY);
-	get_next_line(i, &p);
-	while (n++ < 5)
-	{
-		get_next_line(i, &p);
-		printf("%s\n", p);
-	}
+	char str[50];
+	
+	fd = open("text.txt", O_RDONLY);
+	//read(fd, str, 20); 
+	//str[20] = '\0';
+	get_next_line(fd, &p);
+	printf("%s\n", p);
+	
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -3,20 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vinograd <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:13 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/14 15:12:48 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/16 20:03:09 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1024
+# define BUFF_SIZE 10
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "libft.h"
 
 int		get_next_line(const int fd, char **line);
 
