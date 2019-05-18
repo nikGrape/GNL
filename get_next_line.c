7bcbb7555027f93a7bbb748043653590e72d6f19
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-05-17 21:55:59 -0700

    MAY 17

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:03 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/17 20:36:35 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/17 21:44:51 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 int		get_next_line(const int fd, char **line)
 {
-	char	*p_n;
-	char	*tmp;
-	char	*str;
-	char	buf[BUFF_SIZE + 1];
-	int		ret;
-	int		len;
+	char			*p_n;
+	char			*tmp;
+	char			*str;
+	char			buf[BUFF_SIZE + 1];
+	static char		rest[BUFF_SIZE];
+	int				ret;
+	int				len;
+	int 			rest_len;
 
+	rest_len = ft_strlen(rest);
 	ret = 0;
 	len = 0;
-	str = ft_strnew(1);
+	str = ft_strnew(rest_len);
+	ft_strcat(str, rest);
 	p_n = NULL;
-	while (read(fd, buf, BUFF_SIZE) > 0)
+	while (read(fd, buf, BUFF_SIZE) != 0)
 	{
 		buf[BUFF_SIZE] = '\0';
 		if ((p_n = ft_strchr(buf, '\n')) != 0)
-			ft_strclr(p_n);
+		{
+			ret = 1;
+			ft_strcpy(rest, ++p_n);
+			ft_strclr(--p_n);
+		}
 		tmp = str;
 		len = ft_strlen(str);
 		str = ft_strnew(len + BUFF_SIZE + 1);
+		if (str == NULL)
+			return (-1);
 		ft_strcpy(str, tmp);
 		ft_strdel(&tmp);
 		ft_strcat(str, buf);
@@ -43,20 +53,21 @@ int		get_next_line(const int fd, char **line)
 	*line = str;
 	return (ret);
 }
-/*
+
 int		main(void)
 {
 	int		fd;
 	char	*p;
 	int		i;
+	int		ret = 1;
 
 	i = 0;
 	fd = open("text.txt", O_RDONLY);
-	while (i < 5)
+	while (ret > 0)
 	{
-		get_next_line(fd, &p);
-		printf("%s\n", p);
+		ret = get_next_line(fd, &p);
+		printf("%s%d\n", p, ret);
 		i++;
 	}
-}*/
+}
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:13 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/17 13:43:13 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/17 21:55:10 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 10
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "libft.h"
-#include <string.h>
+# define BUFF_SIZE 11
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "libft.h"
+# include <string.h>
 
 int		get_next_line(const int fd, char **line);
 
