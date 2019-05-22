3d1c122674d67324194bd09f43df1c802dcf9847
Nikolai Vinogradov <vinograd@e1z3r4p21.42.us.org>
2019-05-21 17:16:29 -0700

    MAY 21

--- get_next_line.c
+++ get_next_line.c
@@ -6,68 +6,74 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 21:56:03 by vinograd          #+#    #+#             */
-/*   Updated: 2019/05/17 21:44:51 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/05/21 17:11:49 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	char			*p_n;
 	char			*tmp;
 	char			*str;
 	char			buf[BUFF_SIZE + 1];
 	static char		rest[BUFF_SIZE];
-	int				ret;
-	int				len;
-	int 			rest_len;
+	int				r;
 
-	rest_len = ft_strlen(rest);
-	ret = 0;
-	len = 0;
-	str = ft_strnew(rest_len);
-	ft_strcat(str, rest);
 	p_n = NULL;
-	while (read(fd, buf, BUFF_SIZE) != 0)
+	if (fd < 0)
+		return (-1);
+	if ((p_n = ft_strchr(rest, '\n')) != NULL)
 	{
-		buf[BUFF_SIZE] = '\0';
-		if ((p_n = ft_strchr(buf, '\n')) != 0)
+		str = ft_strsub(rest, 0, p_n - rest);
+		ft_strcpy(rest, ++p_n);
+	}
+	else
+	{
+		str = ft_strnew(ft_strlen(rest) + 1);
+		ft_strcat(str, rest);
+		ft_strclr(rest);
+	}
+	while (p_n == 0 && ((r = read(fd, buf, BUFF_SIZE))))
+	{
+		buf[r] = '\0';
+		if ((p_n = ft_strchr(buf, '\n')) != NULL)
 		{
-			ret = 1;
 			ft_strcpy(rest, ++p_n);
 			ft_strclr(--p_n);
 		}
 		tmp = str;
-		len = ft_strlen(str);
-		str = ft_strnew(len + BUFF_SIZE + 1);
-		if (str == NULL)
+		if (!(str = ft_strnew(ft_strlen(tmp) + BUFF_SIZE + 1)) || r < 0)
 			return (-1);
-		ft_strcpy(str, tmp);
+		ft_strcat(str, tmp);
 		ft_strdel(&tmp);
 		ft_strcat(str, buf);
-		if (p_n != 0)
-			break ;
 		ft_strclr(buf);
+		if (p_n != NULL || r == 0)
+			break ;
 	}
 	*line = str;
-	return (ret);
+	//if (r < 0)
+	//	return (-1);
+	return (ft_strlen(str) ? 1 : 0);
 }
 
-int		main(void)
+int			main(void)
 {
 	int		fd;
 	char	*p;
 	int		i;
-	int		ret = 1;
+	int		ret;
 
 	i = 0;
+	ret = 1;
 	fd = open("text.txt", O_RDONLY);
 	while (ret > 0)
 	{
 		ret = get_next_line(fd, &p);
-		printf("%s%d\n", p, ret);
+		printf("%s\t%d\n", p, ret);
 		i++;
 	}
+	close(fd);
 }
-
